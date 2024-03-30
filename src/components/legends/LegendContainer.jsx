ac838482e6151895a243806281fc93a4f620d9dc
Muhammet Erol <muhammederol971@gmail.comgit config --global user.email muhammederol971@gmail.com>
2024-03-30 01:33:19 +0100

    bootstrap card created and populated data to UI.

--- src/components/legends/LegendContainer.jsx
+++ src/components/legends/LegendContainer.jsx
@@ -1,6 +1,31 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+import { Container, Row, Col } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { data } from "../../helpers/data";
 
 const LegendContainer = () => {
-    return <div>LegendContainer</div>
-}
+  return (
+    <div>
+      <Form.Control type="search" placeholder="Search a Legend" />
+      <Container>
+        <Row xs={1} md={2} lg={3}>
+          {data.map((legends) => (
+            <Col>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src={legends.img} />
+                <Card.Header>
+                  <Card.Title>{legends.name}</Card.Title>
+                </Card.Header>
+                <ul className="m-auto">{legends.statistics.map((item => <li className="list-unstyled h5 text-start">{item}</li>))}</ul>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
 export default LegendContainer;
